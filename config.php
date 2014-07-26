ad63acae82ddb142b588cc5a6255796a7ec9cebc
Don Myers <donmyers@me.com>
2014-07-26 10:25:45 -0400

    init with my changes

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_config = array(
+  'server'   => 'localhost', // Usually localhost
+  'database' => 'tst04', // The database name
+  'username' => 'tst04_admin', // The database username
+  'password' => 'r7EdrsmfhRkgNEaf', // The database password
+  'verbose' => FALSE // If true errors will be shown
+);
+
+$api_login = array('api','d9729feb74992cc3482b350163a1a010');
+$use_cookie = TRUE;
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require('config.php');
+require('lib/arrest-mysql.php');
+
+if ($use_cookie) {
+	if ($_COOKIE['username'] !== $api_login[0] || $_COOKIE['password'] !== $api_login[1]) {
+		header('Content-type: application/json');
+		die('{"error":{"message":"Not Found","code":404}}');
+	}
+}
+
+try {
+	/**
+	* Note: You will need to provide a base_uri as the second param if this file
+	* resides in a subfolder e.g. if the URL to this file is http://example.com/some/sub/folder/index.php
+	* then the base_uri should be "some/sub/folder"
+	*/
+	$arrest = new ArrestMySQL($db_config,'arrest');
+
+	/**
+	* By default it is assumed that the primary key of a table is "id". If this
+	* is not the case then you can set a custom index by using the
+	* set_table_index($table, $field) method
+	*/
+	//$arrest->set_table_index('my_table', 'some_index');
+
+	$arrest->rest();
+
+} catch (Exception $e) {
+	echo $e;
+}
